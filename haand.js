d069ed27ebcadd1744822464e5b9ac63fe4a94ec
Jonathan Theodor Ousted Perrier <4510396@RTS-365.dk>
2018-01-28 19:10:01 +0100

    rettelser af listen til søge funktion

--- haand.js
+++ haand.js
@@ -1,102 +1,102 @@
-// JavaScript Document
+// // JavaScript Document
 
-let listByer = ['Hovedstads Området', 'Aarhus', 'Odense', 'Aalborg', 'Esbjerg', 'Randers', 'Kolding', 'Horsens', 'Vejle', 'Roskilde', 'Herning', 'Hørsholm', 'Helsingør', 'Silkeborg', 'Næstved', 'Fredericia', 'Viborg', 'Køge', 'Holstebro', 'Taastrup', 'Slagelse', 'Hillerød', 'Holbæk', 'Sønderborg'];
-let listHaand = ['VVS', 'Tømrer', 'Maler', 'Murer', 'Anlægsgartner', 'Brolægger', 'Elektriker', 'Entrepenør', 'Gulvsliber', 'Isolatør', 'Kloakmester', 'Snedker'];
-let listByerLink = ['Hovedstads_Omraadet', 'Aarhus', 'Odense', 'Aalborg', 'Esbjerg', 'Randers', 'Kolding', 'Horsens', 'Vejle', 'Roskilde', 'Herning', 'Hoersholm', 'Helsingoer', 'Silkeborg', 'Naestved', 'Fredericia', 'Viborg', 'Koege', 'Holstebro', 'Taastrup', 'Slagelse', 'Hilleroed', 'Holbaek', 'Sonderborg'];
-
-
-let element = document.querySelector('main');
-let lavSekt = document.createElement('section');
-lavSekt.setAttribute('class', 'clearfix');
+// let listByer = ['Hovedstads Området', 'Aarhus', 'Odense', 'Aalborg', 'Esbjerg', 'Randers', 'Kolding', 'Horsens', 'Vejle', 'Roskilde', 'Herning', 'Hørsholm', 'Helsingør', 'Silkeborg', 'Næstved', 'Fredericia', 'Viborg', 'Køge', 'Holstebro', 'Taastrup', 'Slagelse', 'Hillerød', 'Holbæk', 'Sønderborg'];
+// let listHaand = ['VVS', 'Tømrer', 'Maler', 'Murer', 'Anlægsgartner', 'Brolægger', 'Elektriker', 'Entrepenør', 'Gulvsliber', 'Isolatør', 'Kloakmester', 'Snedker'];
+// let listByerLink = ['Hovedstads_Omraadet', 'Aarhus', 'Odense', 'Aalborg', 'Esbjerg', 'Randers', 'Kolding', 'Horsens', 'Vejle', 'Roskilde', 'Herning', 'Hoersholm', 'Helsingoer', 'Silkeborg', 'Naestved', 'Fredericia', 'Viborg', 'Koege', 'Holstebro', 'Taastrup', 'Slagelse', 'Hilleroed', 'Holbaek', 'Sonderborg'];
 
 
-for (let i = 0; i < listByer.length; i++) {
+// let element = document.querySelector('main');
+// let lavSekt = document.createElement('section');
+// lavSekt.setAttribute('class', 'clearfix');
 
 
-	if (i % 4 == 0) {
-		element.appendChild(lavSekt);
-		lavSekt = document.createElement('section');
-		lavSekt.setAttribute('class', 'clearfix');
-	}
-
-	//laver artiklerne i forhold til mængden af byer
-
-	let sektionen = document.createElement('article');
-	sektionen.setAttribute('class', 'col3');
+// for (let i = 0; i < listByer.length; i++) {
 
 
-	//laver h2'erne
+// 	if (i % 4 == 0) {
+// 		element.appendChild(lavSekt);
+// 		lavSekt = document.createElement('section');
+// 		lavSekt.setAttribute('class', 'clearfix');
+// 	}
 
-	let titel = document.createElement('h2');
+// 	//laver artiklerne i forhold til mængden af byer
 
-	//giver h2'eren den tilsvarende titel
-
-	let titelText = document.createTextNode(listByer[i]);
-
-	//apender titlen til h2'en
-
-	titel.appendChild(titelText);
-
-
-	//gemmer h2'eren i artiklen
-
-	sektionen.appendChild(titel);
+// 	let sektionen = document.createElement('article');
+// 	sektionen.setAttribute('class', 'col3');
 
 
-	//laver en un ordered list
+// 	//laver h2'erne
 
-	let uListe = document.createElement('ul');
-	uListe.setAttribute('class', 'hidden');
+// 	let titel = document.createElement('h2');
 
-	//laver list items'ne til den UL
+// 	//giver h2'eren den tilsvarende titel
 
-	for (let j = 0; j < listHaand.length; j++) {
+// 	let titelText = document.createTextNode(listByer[i]);
 
-		let listeItem = document.createElement('li');
-		let linkTag = document.createElement('a');
-		linkTag.setAttribute('href', `/by/${listByerLink[i]}/${listHaand[j]}.html`)
-		let linkTekst = document.createTextNode(listHaand[j]);
+// 	//apender titlen til h2'en
 
-		listeItem.appendChild(linkTag);
-		linkTag.appendChild(linkTekst);
-		uListe.appendChild(listeItem);
-
-	}
-
-	//putter den UL ind i Sektionen
-
-	sektionen.appendChild(uListe);
-
-	lavSekt.appendChild(sektionen);
-	console.log(lavSekt);
-}
+// 	titel.appendChild(titelText);
 
 
-element.appendChild(lavSekt);
+// 	//gemmer h2'eren i artiklen
+
+// 	sektionen.appendChild(titel);
 
 
+// 	//laver en un ordered list
+
+// 	let uListe = document.createElement('ul');
+// 	uListe.setAttribute('class', 'hidden');
+
+// 	//laver list items'ne til den UL
+
+// 	for (let j = 0; j < listHaand.length; j++) {
+
+// 		let listeItem = document.createElement('li');
+// 		let linkTag = document.createElement('a');
+// 		linkTag.setAttribute('href', `/by/${listByerLink[i]}/${listHaand[j]}.html`)
+// 		let linkTekst = document.createTextNode(listHaand[j]);
+
+// 		listeItem.appendChild(linkTag);
+// 		linkTag.appendChild(linkTekst);
+// 		uListe.appendChild(listeItem);
+
+// 	}
+
+// 	//putter den UL ind i Sektionen
+
+// 	sektionen.appendChild(uListe);
+
+// 	lavSekt.appendChild(sektionen);
+// 	console.log(lavSekt);
+// }
+
+
+// element.appendChild(lavSekt);
 
 
 
-let clickeren = document.querySelectorAll('h2');
-let listen = document.querySelectorAll('ul');
-let clickliste = [];
 
 
-for (let i = 0; i < clickeren.length; i++) {
-
-	clickliste.push(clickeren[i]);
-	clickeren[i].addEventListener('click', show);
-
-}
+// let clickeren = document.querySelectorAll('h2');
+// let listen = document.querySelectorAll('ul');
+// let clickliste = [];
 
 
-function show(e) {
-	let q;
-	console.log(e);
-	q = clickliste.indexOf(e.target);
-	listen[q].classList.toggle('hidden');
+// for (let i = 0; i < clickeren.length; i++) {
 
-}
+// 	clickliste.push(clickeren[i]);
+// 	clickeren[i].addEventListener('click', show);
+
+// }
+
+
+// function show(e) {
+// 	let q;
+// 	console.log(e);
+// 	q = clickliste.indexOf(e.target);
+// 	listen[q].classList.toggle('hidden');
+
+// }
 
 
